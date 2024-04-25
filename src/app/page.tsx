024b9ccceda18144d81557ee41251d234747349b
Ryan Eldridge <eldridgerdev@gmail.com>
2024-04-25 10:50:56 -0600

    add post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import { db } from "~/server/db";
 export default async function HomePage() {
     const posts = await db.query.posts.findMany();
 
-    console.log(posts);
     return (
         //<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <main className="">
+            {posts.map((post) => (
+                <div key={post.id}>
+                    <h2>{post.name}</h2>
+                </div>
+            ))}
             Hello (App in progress)
         </main>
     );
