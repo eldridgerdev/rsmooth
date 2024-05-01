da56ffaf1d1c971baf72bc6bd2e400b43e9e8c8a
Ryan Eldridge <eldridgerdev@gmail.com>
2024-05-01 14:15:34 -0600

    add some stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,12 +10,27 @@ export default async function HomePage() {
     return (
         //<main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <main className="">
-            {posts.map((post) => (
-                <div key={post.id}>
-                    <h2>{post.name}</h2>
-                </div>
-            ))}
-            Hello (App in progress)
+            <section className="bg-gradient pb-16">
+                {posts.map((post) => (
+
+                    <div key={post.id}>
+                        <h2>{post.name}</h2>
+                    </div>
+                ))}
+                o Hello (App in progress)
+            </section>
+
+            <section className="bg-primary">
+
+            </section>
+            <section className="ss-style-doublediagonal">
+
+                <div className="text-primary-foreground">test</div>
+            </section>
+            <div className="">
+
+
+            </div>
         </main>
     );
 }
